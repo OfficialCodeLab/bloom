a612066699fe33209a74afee9e8f3d91ea5525f0
Courtney Jooste <courtney@codelab.io>
2016-05-09 06:51:52 -0700

    Created Menu

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
 	actions: {
 		toggleMenu: function () {
-			this.controller.toggleProperty('menuOpen');
+			if(this.controller.get('menuOpen')){
+   				Ember.$('#menu-overlay').fadeOut("slow");
+				this.controller.toggleProperty('menuOpen');
+			} else{
+   				Ember.$('#menu-overlay').fadeIn("slow");
+				this.controller.toggleProperty('menuOpen');
+			}
 		}
-	}
+	}/*,
+	
+	setupController: function(controller, model) {
+		this.controller.set('menuOpen', false)
+		console.log("TEST" + this.controller.get('menuOpen'));
+		Ember.$('#menu-overlay').fadeOut("slow");
+		
+
+  	}*/
 });
