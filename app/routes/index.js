55643a86528d2717bcd51672b85d282b2aaa972e
Carl Eiserman <carleiserman@gmail.com>
2016-05-14 00:01:00 +0200

    Added category skins, for use with models.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+init: function(){
+
+},
+
+
 beforeModel: function() {
+
+  // var cw = this.get('.category-block').width();
+  // Ember.$('.category-block').css({'height':cw+'px'});
+  // this.get('.category-block').css({'height':'300px'});
+
+
+
 	var sesh = this.get("session").fetch().catch(function() {});
 	if(!this.get('session.isAuthenticated')){
       this.transitionTo('login');
     }
     return sesh;
   },
+
+
   actions: {
     
     // willTransition(){     
