20a62347df085356e2f0358961f4d45712fe097a
Courtney Jooste <courtney@codelab.io>
2016-05-23 12:01:30 +0200

    Set up components for vendor login/signup

--- app/components/vendor-login.js
+++ app/components/vendor-login.js
@@ -1,17 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	showButtons: '',
 	login: '',
 	signUp: '',
 
 	actions: {
 		loginRoute: function (){
+			this.set('showButtons', true);
 			this.set('login', true);
 		},
 		signupRoute: function(){
+			this.set('showButtons', true);
 			this.set('signUp', true);
 		},
 		backBtn: function (){
+			this.set('showButtons', false);
 			this.set('login', false);
 			this.set('signup', false);
 
