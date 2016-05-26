b334ca48e8565ebc8e133f39fc667495c80fc23e
Carl Eiserman <carleiserman@gmail.com>
2016-05-26 12:31:52 +0200

    Experimental: Moved profile image to index

--- app/routes/index.js
+++ app/routes/index.js
@@ -16,7 +16,16 @@ init: function(){
   },
 
   model() {
-    return this.store.peekRecord('user', this.get("session").content.currentUser.id);
+    //Before creating the record, clear the DS Store
+    this.store.unloadAll('userext');
+      let _id = this.get("session").content.currentUser.id + "";
+      let imgStr = "http://graph.facebook.com/" + this.get("session").content.currentUser.id + "/picture?type=large";
+    return Ember.RSVP.hash({
+        userext: this.store.createRecord('userext', {
+        imgurl: imgStr
+      }),
+        user: this.store.peekRecord('user', _id)
+      });
   },
 
 
