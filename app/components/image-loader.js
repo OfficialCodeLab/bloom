84e787b838ee6a482f25a2ebff446ad06d86ce5d
Carl Eiserman <carleiserman@gmail.com>
2016-05-28 01:31:00 +0200

    Progress on implementing imagesLoaded.js

---
+++ app/components/image-loader.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	didRender(){
+		// this.$('.cat-img-container').imagesLoaded( function() {
+		// 	console.log("Images have all loaded!");
+		// })
+	    this.$().imagesLoaded( function( $images, $proper, $broken ) {
+	      console.log( $images.length + ' images total have been loaded' );
+	      console.log( $broken.length + ' broken images' );
+	    });
+	}
+});
