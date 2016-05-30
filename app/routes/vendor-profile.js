674a2f2f1d9ce33a8defffca478060f77e94a67c
Courtney Jooste <courtney@codelab.io>
2016-05-30 17:15:53 +0200

    Fixed vendor profile scroll position

--- app/routes/vendor-profile.js
+++ app/routes/vendor-profile.js
@@ -4,5 +4,8 @@ export default Ember.Route.extend({
 
 	model(params){
 		return this.store.findRecord('vendor', params.vendor_id);
-	}
+	},
+  	activate: function(){
+  		window.scrollTo(0,0);
+  	},
 });
