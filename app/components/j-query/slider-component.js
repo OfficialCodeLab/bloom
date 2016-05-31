0651e22cc689f77be30be45fa400f948882623f3
Carl Eiserman <carleiserman@gmail.com>
2016-05-31 17:36:36 +0200

    About Page Progress #3 - Added slick slider to about page - Added sample content to slider - Set width and height in styles - Forced font to FontAwesome for slider buttons - Commented out crappy about div - Replaced Slick scss files with css files (VERY IMPORTANT)

--- app/components/j-query/slider-component.js
+++ app/components/j-query/slider-component.js
@@ -1,5 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	
+	didInsertElement: function(){
+		  this.$('.about-slider').slick({
+			arrows: true
+		  });
+
+	},
+	actions: {
+		test: function(){
+			$('.main-slider').slick({
+			    centerMode: true,
+			    centerPadding: '0px',
+			    slidesToShow: 3,
+			    dots: true,
+			    speed: 1200,
+			    slidesToScroll: 1,
+			    infinite: true,
+			    customPaging: function(slick, index) {
+			        return $('.thumbnails').eq(index).find('img').prop('outerHTML');
+			    },
+			    responsive: [{
+			        breakpoint: 640,
+			        settings: {
+			            arrows: false,
+			            dots: true,
+			            centerMode: false,
+			            slidesToShow: 1,
+			            customPaging: function(slick, index) {
+			                return '<button type="button" data-role="none">' + (index + 1) + '</button>';
+			            }
+			        }
+			    }]
+			});
+		}
+	}
 });
