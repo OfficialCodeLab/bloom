31c2b7cfe0a5ff60247c5cc7c6b13266598ed568
Carl Eiserman <carleiserman@gmail.com>
2016-06-03 11:03:47 +0200

    Implemented a demo pie chart

--- app/routes/index/mywedding/overview.js
+++ app/routes/index/mywedding/overview.js
@@ -1,4 +1,41 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model(){		
+		let _id = this.get("session").content.currentUser.id + "";
+		return this.store.findRecord('wedding', _id);	
+	},
+	actions: {
+		searchUser(){
+			let _name = this.controller.get('name').toLowerCase();
+			if(_name !== '' && _name !== " "){
+				this.controller.set('responseMessage', "");
+				console.log("SEARCHING FOR: " + _name);
+				let searchResults = [];
+				this.controller.set('searching', true);
+				this.store.query('user',  {}).then((users) =>{
+				  // Do something with `peters`
+				  	users.forEach(function(user){
+						let fullname = user.get('name') + " " + user.get('surname'); 	
+						if(~fullname.toLowerCase().indexOf(_name)){
+							searchResults.pushObject({
+								name: fullname,
+								id: user.get("id")
+							});
+							console.log(fullname);
+						}
+					});
+					if(JSON.stringify(searchResults) === "[]"){
+						this.controller.set('responseMessage', "No Users with that name were found");
+					}
+					this.controller.set('searchResults', searchResults);
+					this.controller.set('searching', false);
+					this.controller.get('scroller').scrollVertical("#searchRes", {duration:800});
+		  			this.store.unloadAll('user');
+				});
+			} else {
+
+			}
+		}
+	}
 });
