c001faca534d2311b85b33da0a90e2810acce287
Carl Eiserman <carleiserman@gmail.com>
2016-06-27 23:37:25 +0200

    Messaging Page #3 - Messages will now post to server, - Still need to send through vendor and client email to form

--- app/routes/messaging.js
+++ app/routes/messaging.js
@@ -1,30 +1,40 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	beforeModel: function() {
-	  	var sesh = this.get("session").fetch().catch(function() {});
-	  	if(!this.get('session.isAuthenticated')){
-	        this.transitionTo('login');
-	      }
-	      return sesh;
+  beforeModel: function() {
+    var sesh = this.get("session").fetch().catch(function() {});
+    if (!this.get('session.isAuthenticated')) {
+      this.transitionTo('login');
+    }
+    return sesh;
+  },
+  model() {
+    return this.store.createRecord('message');
+  },
+  activate: function() {
+    window.scrollTo(0, 0);
+  },
+
+  actions: {
+    sendMessage() {
+      let message = this.store.createRecord('message', {
+        to: this.controller.get('model').get('to'),
+        from: this.controller.get('model').get('from'),
+        subject: this.controller.get('model').get('subject'),
+        html: this.controller.get('model').get('html')
+      });
+      alert("Message sent! I think.");
+      message.save();
+
+      // newContact.save().then(() => {
+      //   this.controller.get('model').set('email', '');
+      //   this.controller.get('model').set('message', '');
+      //   this.controller.get('model').set('responseMessage', 'Message has been sent');
+      // });
     },
-    model() {
-    	//return this.store.findRecord('catItem', params.catItem_id);
-  	},
-  	setupController(controller, model) {
-	   //this._super(controller, model);
-
-	    // controller.set('title', 'Edit library');
-	    // controller.set('buttonLabel', 'Save changes');
-  	},
-  	activate: function(){
-  		window.scrollTo(0,0);
-  	},
-
-	actions: {
-		goBack: function(){
-			window.history.go(-1);
-			//console.log("This works.");
-		}
-	}
+    goBack: function() {
+      window.history.go(-1);
+      //console.log("This works.");
+    }
+  }
 });
