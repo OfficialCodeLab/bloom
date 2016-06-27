0666aa61373631aa5b9cb494aa7ebb4f8fd1efc5
Carl Eiserman <carleiserman@gmail.com>
2016-06-27 14:03:09 +0200

    Progress: Messaging Page

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,44 +1,43 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	beforeModel: function() {
-	var sesh = this.get("session").fetch().catch(function() {});
-	if(!this.get('session.isAuthenticated')){
+  beforeModel: function() {
+    var sesh = this.get("session").fetch().catch(function() {});
+    if (!this.get('session.isAuthenticated')) {
       this.transitionTo('login');
     }
     return sesh;
   },
-  	model() {
-    	return this.store.createRecord('contact');
-  	},
+  model() {
+    return this.store.createRecord('contact');
+  },
 
 
-	actions: {
+  actions: {
 
-		saveContact(newContact) {
-			if(this.controller.get('captchaVerified'))
-			{
-				newContact.save().then(() => {
-	        		this.controller.get('model').set('email', '');
-	        		this.controller.get('model').set('message', '');
-	        		this.controller.get('model').set('responseMessage', 'Message has been sent');
-				});				
-			}
-	    },
+    saveContact(newContact) {
+      if (this.controller.get('captchaVerified')) {
+        newContact.save().then(() => {
+          this.controller.get('model').set('email', '');
+          this.controller.get('model').set('message', '');
+          this.controller.get('model').set('responseMessage', 'Message has been sent');
+        });
+      }
+    },
 
-	    willTransition() {
-	      // rollbackAttributes() removes the record from the store
-	      // if the model 'isNew'
-		  this.controller.get('model').set('responseMessage', '');
-	      this.controller.get('model').rollbackAttributes();
-	    },
-	    closeMessage() {
-	    	this.controller.get('model').set('responseMessage', '');
-	    },
-	    guidedSearch(){	    	
-    		window.scrollTo(0,0);
-			this.transitionTo('guide');
-	    }
+    willTransition() {
+      // rollbackAttributes() removes the record from the store
+      // if the model 'isNew'
+      this.controller.get('model').set('responseMessage', '');
+      this.controller.get('model').rollbackAttributes();
+    },
+    closeMessage() {
+      this.controller.get('model').set('responseMessage', '');
+    },
+    guidedSearch() {
+      window.scrollTo(0, 0);
+      this.transitionTo('guide');
+    }
 
-	}
+  }
 });
