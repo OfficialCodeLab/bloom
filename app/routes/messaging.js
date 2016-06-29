e16d73c020e329b77543d7e9de24a96aab8d4306
Carl Eiserman <carleiserman@gmail.com>
2016-06-29 14:29:26 +0200

    Fixed issue: User email not populated into message field

--- app/routes/messaging.js
+++ app/routes/messaging.js
@@ -8,33 +8,38 @@ export default Ember.Route.extend({
     }
     return sesh;
   },
-  // setupController(controller, model) {
-  //   this._super(controller, model);
-  // },
   model: function(params) {
-    // this.controller.set("modelParams", params.id);
-    // return Ember.RSVP.hash({
-    //   vendor:
+  // this.controller.set("modelParams", params.id);
+  // return Ember.RSVP.hash({
+  //   vendor:
     return this.store.findRecord('vendor', params.vendor_id);
     //
     // });
   },
-  afterModel(){
+  afterModel() {
 
-	},
+  },
+  setupController(controller, model) {
+    this._super(controller, model);
+
+    let _id = this.get("session").get('currentUser').providerData[0].uid + "";
+    this.store.findRecord('user', _id).then((currentUser)=>{
+      this.controller.set('from', currentUser.get('email'));
+    }, ()=>{});
+  },
 
   actions: {
     didTransition: function() {
       let _id = this.get("session").get('currentUser').providerData[0].uid + "";
-      this.store.findRecord('user', _id).then((currentUser)=>{
+      this.store.findRecord('user', _id).then((currentUser) => {
         this.controller.set('from', currentUser.get('email'));
-      }, ()=>{});
+      }, () => {});
     },
 
     sendMessage() {
       //Store the current user in a temporary variable
       let _id = this.get("session").get('currentUser').providerData[0].uid + "";
-      let currentUser = this.store.findRecord('user', _id).then((currentUser)=>{
+      let currentUser = this.store.findRecord('user', _id).then((currentUser) => {
         //Create a message to be sent through
         let message = this.store.createRecord('message', {
           to: this.controller.get('model').get('email'),
@@ -47,7 +52,7 @@ export default Ember.Route.extend({
         // alert("Message sent! I think.\n" + JSON.stringify(this.controller.get('currentMessage')));
         // alert("Message sent! I think.\n" + this.model.get('vendor'));
         message.save();
-  		}, ()=>{});
+      }, () => {});
 
 
     },
