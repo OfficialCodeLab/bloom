f5fbc42ec08b05e58c821c0fac59a4ba4037aa6a
Courtney Jooste <courtney@codelab.io>
2016-10-18 11:44:46 +0200

    Added experimental loading bar to all listings

--- app/controllers/listings.js
+++ app/controllers/listings.js
@@ -2,17 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	masonryRef: '',
+	isLoaded: false,
+	isNotLoaded: Ember.computed.not('isLoaded'),
+	percentLoaded: 0,
+	percentLoadedStyle: 'width: 0%',
 	actions: {
 		storeMasonryRef(ref){
 			this.set('masonryRef', ref);
 		},
-		loadedImg(){
-			try{
-			  	var $container = this.get('masonryRef');
-			  	$container.layout();				
-			} catch(ex){}
-			//console.log("THIS WORKS");
-		},
+		// loadedImg(){
+		// 	try{
+		// 	  	var $container = this.get('masonryRef');
+		// 	  	$container.layout();				
+		// 	} catch(ex){}
+		// 	//console.log("THIS WORKS");
+		// },
 	    removedFavourite(){
 	    	//console.log("TESTERINO");
 		  	var $container = this.get('masonryRef');
--- app/routes/listings.js
+++ app/routes/listings.js
@@ -5,6 +5,8 @@ const PAGE_SIZE = 10;
 export default Ember.Route.extend({
   startAt: null,
   endAt: null,
+  loadAmount: 0,
+  loadCount: 0,
 	beforeModel: function() {
 	  	var sesh = this.get("session").fetch().catch(function() {});
 	  	if(!this.get('session.isAuthenticated')){
@@ -20,7 +22,9 @@ export default Ember.Route.extend({
         this.resetIndexes();
       }
       //let sorted = items.sortBy('name');
-      	return items.slice(this.get('startAt'), this.get('endAt'));
+      this.resetLoadCount();
+
+      return items.slice(this.get('startAt'), this.get('endAt'));
     });
   },
 
@@ -41,7 +45,9 @@ export default Ember.Route.extend({
       } else {        
         this.resetIndexes();
       }
-      console.log("START AT: " + this.get('startAt') + "\nEND AT: " + this.get('endAt'));
+      this.resetLoadCount();
+
+      //console.log("START AT: " + this.get('startAt') + "\nEND AT: " + this.get('endAt'));
 
       this.refresh();
     },
@@ -57,9 +63,39 @@ export default Ember.Route.extend({
           this.set('endAt', items.get('length'));
         }
       } 
-      console.log("START AT: " + this.get('startAt') + "\nEND AT: " + this.get('endAt'));
+      //console.log("START AT: " + this.get('startAt') + "\nEND AT: " + this.get('endAt'));
+      this.resetLoadCount();
 
       this.refresh();
+    },
+    loadedImg: function() {     
+      let c = this.get('loadCount');
+      let la = this.get('loadAmount');
+      c++;
+      let percentLoaded = (c / la) * 100;
+      percentLoaded = parseInt(percentLoaded);
+      let percentLoadedStyle = 'width: ' + percentLoaded + '%';
+      this.controller.set('percentLoaded', percentLoaded);
+      this.controller.set('percentLoadedStyle', percentLoadedStyle);
+      //Ember.$("#loading-bar").style.width = "60%";
+      //console.log(percentLoaded);
+      
+      //} else {   
+      console.log("loaded image");
+      this.set('loadCount', c);
+
+     // }
+     if(c >= la){
+        console.log("loading complete");
+        Ember.$('#masonry-items').fadeIn("fast");
+        Ember.$('#loading-spinner').fadeOut("fast");
+      }
+      try{
+          var $container = this.controller.get('masonryRef');
+          $container.layout();        
+      } catch(ex){}
+
+
     }
 
   },
@@ -73,5 +109,15 @@ export default Ember.Route.extend({
         this.set('endAt', items.get('length'));
       }
   },
+  resetLoadCount: function (){
+      try{
+         this.controller.set('isLoaded', false);
+         Ember.$('#masonry-items').fadeOut(0);
+         Ember.$('#loading-spinner').fadeIn(0);
+      } catch(ex){}
+      let loadAmount = this.get('endAt') - this.get('startAt');
+      this.set('loadAmount', loadAmount);
+      this.set('loadCount', 0);    
+  }
   	  
 });
