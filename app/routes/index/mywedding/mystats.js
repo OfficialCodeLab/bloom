062357782bd0d350854685541f6c71d5476fdf92
Courtney Jooste <courtney@codelab.io>
2016-11-15 13:29:09 +0200

    Fixed gender selection

--- app/routes/index/mywedding/mystats.js
+++ app/routes/index/mywedding/mystats.js
@@ -40,6 +40,23 @@ export default Ember.Route.extend({
 
 
 		},
+		selectGender(gender){
+			let _id = this.get("session").get('currentUser').providerData[0].uid + "";
+			let user = this.store.peekRecord('user', _id);
+			if(gender === 'male'){
+				user.set('isFemale', false);
+			} else{
+				user.set('isFemale', true);
+			}
+			user.set('hasGender', true);
+			user.save();
+			let stats = this.store.createRecord('userstat', {
+				id: _id,
+				name: user.get('name') + " " + user.get('surname')
+			});
+			stats.save();
+			this.transitionTo('index.mywedding.mystats');
+		},
 		error: function(error) {
 			this.transitionTo('index.mywedding.selectgender');
 	    },
