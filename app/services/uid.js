763454f7f626945da267dd20eaf19baeb63f9ca9
Courtney Jooste <courtney@codelab.io>
2017-06-22 15:57:52 +0200

    Removed testing code from uid service

--- app/services/uid.js
+++ app/services/uid.js
@@ -8,7 +8,6 @@ export default Ember.Service.extend({
         if (this.get('session.provider') === "password") {
           return this.get('session.currentUser').uid;
         } else {
-          console.log(this.get('session.currentUser'));
           return this.get('session.currentUser').providerData[0].uid;
         }
       } else {
