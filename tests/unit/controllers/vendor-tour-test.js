11629b8cd7c38212741680bd7be02407c2a64ba9
Courtney Jooste <courtney@codelab.io>
2017-07-18 14:16:37 +0200

    Vendor tour enhancements: - Fixed masonry - Added vendor logos - Added hover styles for logos

---
+++ tests/unit/controllers/vendor-tour-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:vendor-tour', 'Unit | Controller | vendor tour', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
