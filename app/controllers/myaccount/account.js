c85755ba728eb692dc35363d3df579dda3bf1a04
Courtney Jooste <courtney@codelab.io>
2017-08-10 12:51:30 +0200

    Changed default state of account page, added placeholder image

--- app/controllers/myaccount/account.js
+++ app/controllers/myaccount/account.js
@@ -4,5 +4,6 @@ import format from 'ember-moment/computeds/format';
 export default Ember.Controller.extend({
 	moment: Ember.inject.service(),
 	dateCurrent: format(),
+	showAccount: true,
 	notifications: Ember.inject.service('notification-messages')
 });
